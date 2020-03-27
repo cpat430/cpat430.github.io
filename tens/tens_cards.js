eca9487ca585ad71b50cbb65001f3bd92f3eea56
Charles <cpat430@aucklanduni.ac.nz>
2020-03-28 12:24:57 +1300

    allows prompt of user to choose trump off of first 5 cards

--- tens/tens_cards.js
+++ tens/tens_cards.js
@@ -1,3 +1,13 @@
+// create a readline interface for reading input from user
+const prompt = require('prompt');
+
+prompt.start();
+
+function onErr(err) {
+    console.log(err);
+    return 1;
+}
+
 // create the suits and the card values to create a deck of cards
 let suits = ['Spades', 'Clubs', 'Diamonds', 'Hearts'];
 let values = [2,3,4,5,6,7,8,9,10,'Jack', 'Queen', 'King', 'Ace'];
@@ -9,7 +19,7 @@ class Card {
     }
 
     to_string() {
-        return this.value + " " + this.suit;
+        return this.value + " of " + this.suit;
     }
 }
 
@@ -39,10 +49,10 @@ class Deck {
         return this.deck;
     }
 
-    deal() {
+    deal(cards) {
         let hand = [];
 
-        while (hand.length < 13) {
+        while (hand.length < cards) {
             hand.push(this.deck.pop());
         }
 
@@ -56,25 +66,106 @@ class Player {
         this.hand = [];
     }
 
+    get_trump(_callback) {
+
+        var trump;
+
+        // deal five cards to the first player
+        p1.hand = deck.deal(5);
+
+        // let the player choose the trump
+        const properties = [
+            {
+                name: 'trump_in',
+                validator: /^[S|s|C|c|D|d|H|h]{1,1}$/,
+                warning: 'trump can only be [s]pades, [c]lubs, [d]iamonds, [h]earts'
+            },
+        ];
+
+        prompt.get(properties, function (err, result) {
+            if (err) { 
+                return onErr(err); 
+            }
+            console.log('Command-line input received:');
+            console.log('  Trump: ' + result.trump_in);
+        });
+
+        switch(trump_in) {
+            case 'S':
+            case 's':
+                trump = suits[0];
+                valid = true;
+                break;
+            case 'C':
+            case 'c':
+                trump = suits[1];
+                valid = true;
+                break;
+            case 'D':
+            case 'd':
+                trump = suits[2];
+                valid = true;
+                break;
+            case 'H':
+            case 'h':
+                trump = suits[3];
+                valid = true;
+                break;
+        }
+
+        _callback();
+
+        return trump;
+    }
+
     // can play a card
     play(card) {
         console.log('Played ' + card.to_string());
     }
 }
 
+// create the playing card deck
 let deck = new Deck();
-deck.create_deck(suits, values)
+deck.create_deck(suits, values);
 
-// put cards in hand
-let player_1 = new Player(1);
-player_1.hand = deck.deal();
+// shuffle the deck
+deck.shuffle_deck();
 
-player_1.play(player_1.hand[0]);
+// create the four players
+let p1 = new Player(1),
+    p2 = new Player(2),
+    p3 = new Player(3),
+    p4 = new Player(4);
 
-let players = 4,
+let players = [p1,p2,p3,p4];
+
+let trump = null,
+    trump_in,
     tricks = 0,
     game = {
-        players: [],
         game_over: false
-    },
-    i = players;
+    };
+
+// start the game...
+// choose the trump
+p1.get_trump(function() {
+    console.log('done');
+});
+
+// we will keep track of the number of rounds so that we can 
+// rotate who is choosing the trump
+
+if (trump != null) {
+    console.log('The trump is now ' + trump);
+}
+
+
+
+// put 13 cards in players hands
+for (let p of players) {
+    p.hand = deck.deal(13);
+}
+
+p1.play(p1.hand[0]);
+
+
